7d6b3a47cf4c58c153b443c9ad9e0d8e072f0197
georg-cantor <qin.dongliang.nudt@gmail.com>
2021-06-16 22:00:40 +0800

    exercise 1.6 of part1-continued

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,4 +1,28 @@
-import React, { useState } from 'react'
+import React, { useImperativeHandle, useState } from 'react'
+
+const Header = ({header}) => {
+  return (
+    <div>
+      <h1>{header}</h1>
+    </div>
+  )
+}
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick = {handleClick} >
+      {text}
+    </button>
+  )
+}
+
+const Display = ({name, counter}) => {
+  return (
+    <div>
+      {name} {counter}
+    </div>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -6,9 +30,29 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodFeedback = () => setGood(good + 1)
+  const handleNeutralFeedback = () => setNeutral(neutral + 1)
+  const handleBadFeedback = () => setBad(bad + 1)
+
   return (
     <div>
-      code here
+      <Header header="give feedback" />
+      <Button 
+        handleClick={handleGoodFeedback}
+        text='good' 
+      />
+      <Button 
+        handleClick={handleNeutralFeedback}
+        text='neutral' 
+      />
+      <Button 
+        handleClick={handleBadFeedback}
+        text='bad' 
+      />
+      <Header header="statistics" />
+      <Display name="good" counter={good} />
+      <Display name="neutral" counter={neutral} />
+      <Display name="bad" counter={bad} />
     </div>
   )
 }
