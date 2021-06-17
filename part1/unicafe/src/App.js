f1d3f6f1c68866dfe9d9ef1cb420900b5b046c95
georg-cantor <qin.dongliang.nudt@gmail.com>
2021-06-17 08:29:25 +0800

    exercise 1.8 of part1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -24,12 +24,7 @@ const Display = ({name, counter}) => {
   )
 }
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+const Statistics = ({good, neutral, bad}) => {
   const calcAll = (good, neutral, bad) => good + neutral + bad
   const calcAvg = (good, neutral, bad) => {
     const all = calcAll(good, neutral, bad)
@@ -41,6 +36,24 @@ const App = () => {
     const positiveRatio = good/all 
     return "" + positiveRatio*100 + " %"
   }
+  
+  return (
+    <div>
+      <Display name="good" counter={good} />
+      <Display name="neutral" counter={neutral} />
+      <Display name="bad" counter={bad} />
+      <Display name="all" counter={calcAll(good, neutral, bad)} />
+      <Display name="average" counter={calcAvg(good, neutral, bad)} />
+      <Display name="positive" counter={calcPos(good, neutral, bad)} />
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   const handleGoodFeedback = () => setGood(good + 1)
   const handleNeutralFeedback = () => setNeutral(neutral + 1)
@@ -62,12 +75,7 @@ const App = () => {
         text='bad' 
       />
       <Header header="statistics" />
-      <Display name="good" counter={good} />
-      <Display name="neutral" counter={neutral} />
-      <Display name="bad" counter={bad} />
-      <Display name="all" counter={calcAll(good, neutral, bad)} />
-      <Display name="average" counter={calcAvg(good, neutral, bad)} />
-      <Display name="positive" counter={calcPos(good, neutral, bad)} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
